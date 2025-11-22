210664b7cee56dbe2947d1fff72a9669a75e30bc
Tera-Dark <wjx19825323597@gmail.com>
2025-11-22 13:23:33 +0800

    fix: configure correct base path for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Prompt-Hub/',
+  base: process.env.VERCEL ? '/' : '/Prompt-Hub/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
